d5bf21bcd160f55e1a8213d0eb4c806d923151d5
Alexander Demin <bouvens@gmail.com>
2017-01-16 22:12:09 +0600

    Static field moving on React

--- src/components/RedSquares.js
+++ src/components/RedSquares.js
@@ -1,13 +1,48 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import style from './RedSquares.less'
 
-export default class RedSquares extends React.Component {
+const Hero = (props) => <div
+    className={style.hero}
+    style={{
+        top: `${props.top}px`,
+        left: `${props.left}px`,
+    }}
+/>
+
+Hero.propTypes = {
+    top: PropTypes.number.isRequired,
+    left: PropTypes.number.isRequired,
+}
+
+const Field = (props) => <div className={style.field}>
+    {props.children}
+</div>
+
+Field.propTypes = {
+    children: PropTypes.object,
+}
+
+Field.defaultProps = {
+    children: null,
+}
+
+export default class RedSquares extends Component {
+    handleMouseMove (e) {
+        // console.log(e.pageX, e.pageY)
+    }
+
     render () {
         return (
-            <div className={style.wrapper}>
-                <div className={style.field}>
-                    <div className={style.hero} />
-                </div>
+            <div className={style.wrapper} onMouseMove={this.handleMouseMove}>
+                <Field
+                    ref={(elem) => { this.hero = elem }}
+                >
+                    <Hero
+                        ref={(elem) => { this.hero = elem }}
+                        top={10}
+                        left={20}
+                    />
+                </Field>
             </div>
         )
     }
