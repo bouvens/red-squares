6a3e1a44c7074da8ff793992c7b51d61be507933
Alexander Demin <bouvens@gmail.com>
2017-02-01 23:53:51 +0600

    Project prepared for build

--- nwb.config.js
+++ nwb.config.js
@@ -6,6 +6,7 @@ module.exports = {
                 modules: true,
                 localIdentName: '[name]__[local]__[hash:base64:5]'
             }
-        }
+        },
+        publicPath: ''
     }
 }
