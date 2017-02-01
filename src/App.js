beb5a6d6271d218ab13dd9f361c1b8f17fc991bc
Alexander Demin <bouvens@gmail.com>
2017-02-01 23:43:15 +0600

    Random removing added, Reset button added, sidebar added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import RedSquares from './components/RedSquares'
 const SETTINGS = {
     heroSize: 50,
     threatSize: 30,
-    threatLimit: 5,
-    threatAddTimeout: 2,
+    threatLimit: 10,
+    threatAddTimeout: 1500,
+    threatRemoveProbability: 20,
 }
 
 const App = () => (
--- src/components/RedSquares.js
+++ src/components/RedSquares.js
@@ -21,6 +21,7 @@ export default class RedSquares extends Component {
         threatSize: PropTypes.number.isRequired,
         threatLimit: PropTypes.number.isRequired,
         threatAddTimeout: PropTypes.number.isRequired,
+        threatRemoveProbability: PropTypes.number.isRequired,
     }
 
     state = {
@@ -30,16 +31,7 @@ export default class RedSquares extends Component {
             status: heroStates.normal,
         },
         threats: [],
-        lastTreatTime: performance.now() - this.props.threatAddTimeout,
-    }
-
-    componentDidMount () {
-        document.onmousemove = this.saveMousePos
-
-        this.saveFieldSize()
-        window.onresize = this.saveFieldSize
-
-        setInterval(this.updateFrame, 20)
+        threatsIndex: 0,
     }
 
     static mousePos = {
@@ -54,6 +46,52 @@ export default class RedSquares extends Component {
         }
     }
 
+    static keyPressed = {}
+
+    saveKeyPressed = ({ key }) => {
+        RedSquares.keyPressed = {
+            ...RedSquares.keyPressed,
+            [key]: true,
+        }
+    }
+
+    lastTreatTime = performance.now() - this.props.threatAddTimeout
+
+    componentDidMount () {
+        document.onmousemove = this.saveMousePos
+
+        this.saveFieldSize()
+        window.onresize = this.saveFieldSize
+        window.document.onkeyup = this.saveKeyPressed
+
+        this.startInterval()
+    }
+
+    startInterval = () => {
+        this.setState({
+            interval: setInterval(this.updateFrame, 20)
+        })
+    }
+
+    updateFrame = () => {
+        if (RedSquares.keyPressed.Escape) {
+            RedSquares.keyPressed.Escape = false
+            this.reset()
+            return
+        }
+        this.moveHero()
+        this.moveThreats()
+        this.controlThreats()
+    }
+
+    reset = () => {
+        clearInterval(this.state.interval)
+        this.startInterval()
+        this.setState({
+            threats: [],
+        })
+    }
+
     getHeroStatus = () => {
         const { heroSize, threatSize } = this.props
         const safeLength = (heroSize + threatSize) / 2
@@ -63,12 +101,6 @@ export default class RedSquares extends Component {
         return this.state.threats.some((threat) =>
             Math.abs(threat.x - hero.x + sizeFix) < safeLength && Math.abs(threat.y - hero.y + sizeFix) < safeLength
         ) ? heroStates.trouble : heroStates.normal
-    }
-
-    updateFrame = () => {
-        this.moveHero()
-        this.moveThreats()
-        this.controlThreats()
     }
 
     saveFieldSize = () => {
@@ -114,23 +146,85 @@ export default class RedSquares extends Component {
         })
     }
 
+    processSpeed = (options) => {
+        const { size, lean, canFlyAway } = options
+        let { speed, isOut } = options
+        const isGoingOut = Math.sign(lean) === Math.sign(speed)
+
+        if (!isOut) {
+            if (canFlyAway && isGoingOut) {
+                isOut = true
+            } else {
+                speed = (isGoingOut ? -1 : 1) * (speed || 1)
+            }
+        }
+
+        return {
+            speed,
+            isOut,
+            isAroundField: Math.abs(lean) <= size * 3,
+        }
+    }
+
     beat = (field) => (threat) => {
-        let { x, y, speed } = threat
+        const { x, y, speed } = threat
+        let { isOut, isAroundField } = threat
+        const canFlyAway = Math.random() < 1 / this.props.threatRemoveProbability
 
+        if (x < 0) {
+            const lean = x
+            const processed = this.processSpeed({
+                size: this.props.threatSize,
+                speed: speed.x,
+                lean,
+                isOut,
+                canFlyAway
+            })
+            speed.x = processed.speed
+            isOut = processed.isOut
+            isAroundField = processed.isAroundField
+        }
         const rightBorder = field.width - this.props.threatSize
-        if (x <= 0) {
-            speed.x = Math.abs(speed.x)
-        }
-        if (x >= rightBorder) {
-            speed.x = -Math.abs(speed.x)
+        if (x > rightBorder) {
+            const lean = x - rightBorder
+            const processed = this.processSpeed({
+                size: this.props.threatSize,
+                speed: speed.x,
+                lean,
+                isOut,
+                canFlyAway,
+            })
+            speed.x = processed.speed
+            isOut = processed.isOut
+            isAroundField = processed.isAroundField
         }
 
-        const bottomBorder = field.height - this.props.threatSize
-        if (y <= 0) {
-            speed.y = Math.abs(speed.y)
+        if (y < 0) {
+            const lean = y
+            const processed = this.processSpeed({
+                size: this.props.threatSize,
+                speed: speed.y,
+                lean,
+                isOut,
+                canFlyAway,
+            })
+            speed.y = processed.speed
+            isOut = processed.isOut
+            isAroundField = processed.isAroundField
         }
+        const bottomBorder = field.height - this.props.threatSize
         if (y >= bottomBorder) {
-            speed.y = -Math.abs(speed.y)
+            const lean = y - bottomBorder
+            const processed = this.processSpeed({
+                size: this.props.threatSize,
+                speed: speed.y,
+                lean,
+                isOut,
+                canFlyAway,
+            })
+            speed.y = processed.speed
+            isOut = processed.isOut
+            isAroundField = processed.isAroundField
         }
 
         return {
@@ -138,35 +232,65 @@ export default class RedSquares extends Component {
             x,
             y,
             speed,
+            isOut,
+            isAroundField,
         }
+    }
+
+    addThreat = (threats, field) => {
+        const size = this.props.threatSize
+        let x = Math.round(Math.random() * (field.width - size))
+        let y = 0 - size
+        let speed = {
+            x: Math.round(Math.random() * 8 - 4),
+            y: Math.ceil(Math.random() * 4),
+        }
+
+        if (Math.random() < 0.5) {
+            x = 0 - size
+            y = Math.round(Math.random() * (field.height - size))
+            speed = {
+                x: Math.ceil(Math.random() * 4),
+                y: Math.round(Math.random() * 8 - 4),
+            }
+
+            if (Math.random() < 0.5) {
+                x = field.width
+                speed.x = -speed.x
+            }
+        } else if (Math.random() < 0.5) {
+            y = field.height
+            speed.y = -speed.y
+        }
+        threats.push({
+            id: this.state.threatsIndex,
+            x,
+            y,
+            speed,
+            isOut: false,
+            isAroundField: true,
+        })
+
+        this.lastTreatTime = performance.now()
+        this.setState({
+            threats,
+            threatsIndex: this.state.threatsIndex + 1,
+        })
     }
 
     controlThreats = () => {
         const field = this.state.field
         let threats = this.state.threats.map(this.beat(field))
+        threats = threats.filter((threat) => threat.isAroundField)
 
         if (threats.length < this.props.threatLimit
-            && performance.now() >= this.state.lastTreatTime + this.props.threatAddTimeout * 1000) {
-            let y = 0 - this.props.threatSize
-            let speed = {
-                x: Math.floor(Math.random() * 4),
-                y: Math.floor(Math.random() * 4 + 1),
-            }
-
-            if (Math.random() < 0.5) {
-                y = field.height + this.props.threatSize
-                speed.y = -speed.y
-            }
-            threats.push({
-                x: Math.floor(Math.random() * (field.width - this.props.threatSize + 1)),
-                y,
-                speed,
-            })
-            this.setState({
-                threats,
-                lastTreatTime: performance.now(),
-            })
+            && performance.now() >= this.lastTreatTime + this.props.threatAddTimeout) {
+            this.addThreat(threats, field)
         }
+
+        this.setState({
+            threats
+        })
     }
 
     render () {
@@ -183,8 +307,9 @@ export default class RedSquares extends Component {
                         left={this.state.hero.x}
                         top={this.state.hero.y}
                     />
-                    {this.state.threats.map((threat, index) => (
+                    {this.state.threats.map((threat) => (
                         <Square
+                            key={threat.id}
                             style={style.threat}
                             size={this.props.threatSize}
                             left={threat.x}
@@ -192,10 +317,18 @@ export default class RedSquares extends Component {
                         />
                     ))}
                 </Field>
-                <p>hero.x = {this.state.hero.x}</p>
-                <p>hero.y = {this.state.hero.y}</p>
-                <p>threat.x = {this.state.threats[0] && this.state.threats[0].x}</p>
-                <p>threat.y = {this.state.threats[0] && this.state.threats[0].y}</p>
+                <div className={style.side}>
+                    <button onClick={this.reset}>Reset</button>
+                    <p>hero.x = {this.state.hero.x}</p>
+                    <p>hero.y = {this.state.hero.y}</p>
+                    <p>threat.id = {this.state.threats[0] && this.state.threats[0].id}</p>
+                    <p>threat.x = {this.state.threats[0] && this.state.threats[0].x}</p>
+                    <p>threat.y = {this.state.threats[0] && this.state.threats[0].y}</p>
+                    <p>threat.speed.x = {this.state.threats[0] && this.state.threats[0].speed.x}</p>
+                    <p>threat.speed.y = {this.state.threats[0] && this.state.threats[0].speed.y}</p>
+                    <p>threat.out = {this.state.threats[0] && this.state.threats[0].out ? 'true' : 'false'}</p>
+                    <p>threats.length = {this.state.threats.length}</p>
+                </div>
             </div>
         )
     }
