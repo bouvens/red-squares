8a3db1d6def403d73e7ce6a9ef5048afd24a13b4
Alexander Demin <bouvens@gmail.com>
2017-04-05 15:01:59 +0600

    StateControl updated

--- src/components/StateControl/index.js
+++ src/components/StateControl/index.js
@@ -1,5 +1,5 @@
 export { SettersBlock } from './controls/SettersBlock/SettersBlock'
-export { Connector } from './Connector'
+export { Connector } from './controls/Connector/Connector'
 export { default as Input } from './controls/Input/Input'
 export { default as Radio } from './controls/Radio/Radio'
 export { default as Check } from './controls/Check/Check'
