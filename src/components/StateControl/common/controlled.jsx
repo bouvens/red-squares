cd094f3ce76378f2398dbad403fb8403b77cf0dd
Alexander Demin <bouvens@gmail.com>
2017-07-09 14:26:12 +0600

    React PropTypes made imported from right place

--- src/components/StateControl/common/controlled.jsx
+++ src/components/StateControl/common/controlled.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { noOperation } from './utils'
 
 const controlled = (Child) => class extends React.Component {
--- src/components/StateControl/controls/Check/Check.jsx
+++ src/components/StateControl/controls/Check/Check.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import controlled from '../../common/controlled'
 import { noOperation } from '../../common/utils'
 import './Check.css'
