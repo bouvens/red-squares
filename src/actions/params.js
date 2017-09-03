269f81c8b6398f2d8f1b59ecbd79cedcb656b8a6
Alexander Demin <bouvens@gmail.com>
2017-09-03 20:03:04 +0600

    One action simplified.

--- src/actions/params.js
+++ src/actions/params.js
@@ -1,11 +1,7 @@
 import _ from 'lodash'
 import * as types from '../constants/actionTypes'
 
-export function setState (name, value) {
-    return (dispatch) => {
-        dispatch({
-            type: types.SET_STATE,
-            data: _.set({}, name, value)
-        })
-    }
-}
+export const setState = (name, value) => ({
+    type: types.SET_STATE,
+    data: _.set({}, name, value)
+})
